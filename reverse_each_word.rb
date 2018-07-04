75213aed07095e5c97a5590de8f649fd00e1c777
Jonathan Hong <jonhong777@gmail.com>
2018-07-04 00:54:27 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,8 +3,8 @@ def reverse_each_word(sentence)
   binding.pry
   sentence_array = sentence.split(" ")
   
-  array = sentence_array.collect do |letter|
-    letter.reverse
+  sentence_array.collect do |word|
+    word.reverse
   end
   array.join(" ")
 end
