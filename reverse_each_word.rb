37e151caf82a3c6912c470708800f54484ddafc0
Jonathan Hong <jonhong777@gmail.com>
2018-07-04 00:51:04 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
 require "pry"
 def reverse_each_word(sentence)
+  binding.pry
   sentence_array = sentence.split(" ")
   
   array = sentence_array.collect do |letter|
     letter.reverse
   end
-  binding.pry
   array.join(" ")
 end
